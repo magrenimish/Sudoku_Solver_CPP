ea178bc6f5c8e20acffa5acdf56ba2c80397c987
Nimish Magre <magre.n@northeastern.edu>
2022-11-02 15:49:23 -0400

    Update hough.cpp

--- hough.cpp
+++ hough.cpp
@@ -21,10 +21,7 @@ std::vector<cv::Mat> houghline(string filename)
         if (l[2] > maxx) maxx = l[2];
         if (l[3] > maxy) maxy = l[3];
     }
-    /*cout << "\n" << "minx is " << minx << "\n";
-    cout << "\n" << "maxy is " << maxy << "\n";
-    cout << "\n" << "maxx is " << maxx << "\n";
-    cout << "\n" << "miny is " << miny << "\n";*/
+    
     Mat img;
     float w1 = ((maxy - miny) / 9);
     float h1 = ((maxx - minx) / 9);
@@ -34,14 +31,9 @@ std::vector<cv::Mat> houghline(string filename)
                 Mat imcrop = src(Rect(q, w, h1, w1));
                 Mat resized_up;
                 resize(imcrop, resized_up, Size(28, 28), INTER_LINEAR);
-                
-                        //string img_name = std::to_string(w2+1) + ".jpg";
-                        //imwrite("C:/Users/nimis/source/repos/OpenCV_Course/OpenCV_Course/hough_images/"+img_name, imcrop);
-                        Vec[w2] = imcrop;
+                Vec[w2] = imcrop;
                         w2 += 1;
-                /*imshow("imcrop", imcrop);
-                waitKey(200);*/
-            }
+                }
         } 
     return (Vec);
 }
